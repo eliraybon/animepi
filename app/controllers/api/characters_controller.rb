9eab2dc02749e37b64ecdcabe520f828023f8336
eliraybon <elr0615@gmail.com>
2020-02-13 14:33:39 -0500

    Add pages to character controller

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -1,11 +1,21 @@
 class Api::CharactersController < ApplicationController
 
   def index
-    anime = Anime.find_by(id: params[:anime_id])
-    if anime
-      @characters = anime.characters
+    # if we are getting all the characters for a particular anime...
+    if params[:anime_id]
+      #find all the characters of that anime
+      @anime = Anime.find_by(id: params[:anime_id])
+      @characters = @anime.characters
     else 
-      @characters = Character.all
+      #otherwise, we are going to use a page system, with 10 chars per page
+      #we get the page from the query string, or set it to 1 by default 
+      @page = params[:page] ? params[:page].to_i : 1
+      #using the page, we set range of ids we need to retrieve
+      range = @page * 10 - 9..@page * 10
+      #fetch to characters
+      @characters = Character.where(id: range)
+      #to calculate the last page, we use the id of the last character 
+      @max_page = Character.last.id / 10 + 1
     end
     render :index
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+hunter_x_hunter = Anime.create(
+  title: 'Hunter x Hunter',
+  genre: 'Shonen',
+  episodes: 148,
+  year: 2011
+)
+
+hunter_x_hunter.characters.create(name: 'Gon Freecss')
+hunter_x_hunter.characters.create(name: 'Killua Zoldyck')
+hunter_x_hunter.characters.create(name: 'Kurapika Kurta')
+hunter_x_hunter.characters.create(name: 'Leorio Paladiknight')
+hunter_x_hunter.characters.create(name: 'Hisoka Morrow')
+hunter_x_hunter.characters.create(name: 'Illumi Zoldyck')
+hunter_x_hunter.characters.create(name: 'Chrollo Lucilfer')
+hunter_x_hunter.characters.create(name: 'Tonpa')
+hunter_x_hunter.characters.create(name: 'Bicuit Krueger')
+hunter_x_hunter.characters.create(name: 'Meruem')
+hunter_x_hunter.characters.create(name: 'Komugi')
+
+
+stars_align = Anime.create(
+  title: 'Stars Align',
+  genre: 'Sports / Slice of Life',
+  episodes: 12, 
+  year: 2019
+)
+
+stars_align.characters.create(name: 'Maki Katsuragi')
+stars_align.characters.create(name: 'Toma Shinjo')
+stars_align.characters.create(name: 'Yu')
+
+free = Anime.create(
+  title: 'Free!',
+  genre: 'Sports / Slice of Life',
+  episodes: 36,
+  year: 2014
+)
+
+free.characters.create(name: 'Haru')
+free.characters.create(name: 'Rin')
+free.characters.create(name: 'Makato')
+free.characters.create(name: 'Nagisa')
+free.characters.create(name: 'Rei')
+
+naruto = Anime.create(
+  title: 'Naruto',
+  genre: 'Shonen',
+  episodes: 400,
+  year: 2004
+)
+
+naruto.characters.create(name: 'Naruto Uzumaki')
+naruto.characters.create(name: 'Sasuke Uchiha')
+naruto.characters.create(name: 'Sakura Haruno')
+naruto.characters.create(name: 'Kakashi Hatake')
+naruto.characters.create(name: 'Gaara')
+naruto.characters.create(name: 'Rock Lee')
+naruto.characters.create(name: 'Ino')
+naruto.characters.create(name: 'Chogi')
+naruto.characters.create(name: 'Sheeno')
+naruto.characters.create(name: 'Shikamaru')
+naruto.characters.create(name: 'Konkuro')
+naruto.characters.create(name: 'Temari')
+naruto.characters.create(name: 'Orochimaru')
+naruto.characters.create(name: 'Kabuto')
+naruto.characters.create(name: 'Haku')
