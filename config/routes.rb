54eb4e650eb08c2b1a240a41e680592603a3a220
eliraybon <elr0615@gmail.com>
2020-02-14 15:44:54 -0500

    Add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+  get '/about', to: "static_pages#about"
+  get '/documentation', to: "static_pages#documentation"
+
   namespace :api, defaults: {format: :json} do
     resources :anime, only: [:index, :show] do 
       resources :characters, only: [:index]
