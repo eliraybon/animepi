741dc11cfe9e097b38eb572b976d9679650e86b5
eliraybon <elr0615@gmail.com>
2020-03-14 16:37:53 -0400

    Change script to ES5 syntax

--- app/assets/javascripts/interactive_api.js
+++ app/assets/javascripts/interactive_api.js
@@ -1,9 +1,20 @@
-$(document).ready(() => {
-  $("#req-button").click(() => {
+// $(document).ready(() => {
+//   $("#req-button").click(() => {
+//     const value = $("#req-input").val();
+//     const url = "/api/" + value;
+//     $.ajax({ url }).complete(data => {
+//       console.log(data);
+//       const content = $.parseJSON(data['responseText']);
+//       $("#api-output").text(JSON.stringify(content, null, '\t'));
+//     })
+//   })
+// })
+
+$(document).ready(function () {
+  $("#req-button").click(function () {
     const value = $("#req-input").val();
     const url = "/api/" + value;
-    $.ajax({ url }).complete(data => {
-      console.log(data);
+    $.ajax({ url }).complete(function (data) {
       const content = $.parseJSON(data['responseText']);
       $("#api-output").text(JSON.stringify(content, null, '\t'));
     })
