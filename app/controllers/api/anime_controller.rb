39697d7af3852ada9945ac7a0d393771849162ca
eliraybon <elr0615@gmail.com>
2020-03-15 18:35:57 -0400

    Implement first draft of pagination

--- app/controllers/api/anime_controller.rb
+++ app/controllers/api/anime_controller.rb
@@ -6,10 +6,18 @@ class Api::AnimeController < ApplicationController
 
     if search[:search_query].empty?
       #if there is no order param... Rails will make query SELECT anime.* FROM anime
-      @anime = Anime.all.order(search[:order])
+      anime = Anime.all.order(search[:order])
     else
-      @anime = Anime.where(search[:search_query], *search[:values]).order(search[:order])
+      anime = Anime.where(search[:search_query], *search[:values]).order(search[:order])
     end
+
+    anime_per_page = 10
+    page = params[:page] ? params[:page].to_i : 1
+    @count = anime.count
+    max_page = (@count / anime_per_page) + (@count % anime_per_page == 0 ? 0 : 1)
+    @next_page = page + 1 <= max_page ? page + 1 : nil
+    @prev_page = page - 1 > 0 ? page - 1 : nil
+    @anime = anime.limit(10).offset((page - 1) * anime_per_page)
 
     render :index
   end
